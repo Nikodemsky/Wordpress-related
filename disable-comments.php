9564c30b7973b87d3ba32607b8c1416e3bacf91d
Nikodemsky <Nikodemh@gmail.com>
2025-08-19 01:24:10 +0200

    Create disable-comments.php

---
+++ disable-comments.php
@@ -0,0 +1,40 @@
+// Removes comments completely
+add_action('admin_init', function () {
+
+    global $pagenow;
+    
+    if ($pagenow === 'edit-comments.php' || $pagenow === 'options-discussion.php') {
+        wp_redirect(admin_url());
+        exit;
+    }
+
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php');
+    remove_submenu_page('options-general.php', 'options-discussion.php');
+});
+
+add_action('init', function () {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+});
+
+function remove_comments(){
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+}
+add_action( 'wp_before_admin_bar_render', 'remove_comments' );
